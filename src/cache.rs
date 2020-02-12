e17b7debf5c69d1f1f6bd48cdf16deb24026d978
DrPandemic <DrPandemic@users.noreply.github.com>
2020-02-11 19:52:18 -0500

    Reduce TTL for cached results and add qtype in the cache key

--- src/cache.rs
+++ src/cache.rs
@@ -3,7 +3,7 @@ use lru::LruCache;
 use std::time::{Duration, SystemTime};
 
 pub struct Cache {
-    pub data: LruCache<String, (SystemTime, Message)>,
+    pub data: LruCache<(String, u16), (SystemTime, Message)>,
 }
 
 impl Cache {
@@ -14,37 +14,35 @@ impl Cache {
     }
 
     pub fn get(&mut self, query: &Message) -> Option<Message> {
-        if let Some(question) = query.question() {
-            let name = question.qname().join(".");
-            let (time, message) = self.data.get(&name)?;
-            if time > &SystemTime::now() {
-                let mut response = (*message).clone();
-                response.set_id(query.id());
-                // if let Some(mut rrs) = response.resource_records() {
-                //     if rrs.0.len() > 0 {
-                //         rrs.0[0].set_response_ttl(1);
-                //     }
-                // }
-                Some(response)
-            } else {
-                self.data.pop(&name);
-                None
-            }
+        let question = query.question()?;
+        let key = (question.qname().join("."), question.qtype());
+        let (time, message) = self.data.get(&key)?;
+        let time_difference = SystemTime::now().duration_since(time.clone()).ok()?;
+        if time > &SystemTime::now() {
+            let mut response = (*message).clone();
+            response.set_id(query.id());
+            response.set_response_ttl(time_difference.as_secs() as u32);
+            Some(response)
         } else {
+            self.data.pop(&key);
             None
         }
     }
 
     pub fn put(&mut self, message: &Message) {
-        if let Some((responses, _, _)) = message.resource_records() {
+        if let (Some((responses, _, _)), Some(question)) =
+            (message.resource_records(), message.question())
+        {
             if responses.len() > 0 {
                 let ttl = SystemTime::now()
                     .checked_add(Duration::from_secs(responses[0].ttl as u64))
                     .unwrap_or(SystemTime::now());
                 if ttl > SystemTime::now() {
                     // I think this is wrong. What if the TTLs are different?
-                    self.data
-                        .put(responses[0].name.join("."), (ttl, message.clone()));
+                    self.data.put(
+                        (responses[0].name.join("."), question.qtype()),
+                        (ttl, message.clone()),
+                    );
                 }
             }
         }
